d9155320479c409c9c6940483aff872e526478ab
iwa <220993652+iwa-koiwa@users.noreply.github.com>
2026-01-11 11:27:13 +0900

    readmeの追加など

--- B260111/src/main.rs
+++ B260111/src/main.rs
@@ -1,20 +1,43 @@
+use core::num;
+
 
 fn main() {
-    let mut count = 0;
-    'counting_up:loop {
-        println!("The count:{}",count);
-        let mut remaining = 10;
-        loop {
-            println!("remaing:{}",remaining);
-            if remaining == 9{
-                break;
-            }
-            if count == 2 {
-                break 'counting_up;
-            }
-            remaining -= 1;
-        }
-        count +=1;
+    // let mut count = 0;
+    // 'counting_up:loop {
+    //     println!("The count:{}",count);
+    //     let mut remaining = 10;
+    //     loop {
+    //         println!("remaing:{}",remaining);
+    //         if remaining == 9{
+    //             break;
+    //         }
+    //         if count == 2 {
+    //             break 'counting_up;
+    //         }
+    //         remaining -= 1;
+    //     }
+    //     count +=1;
+    // }
+    // println!("End count is :{}",count);
+    let x =[10,20,30,40,50];
+
+    for element in x{
+        println!("the x is:{}", element );
     }
-    println!("End count is :{}",count);
+
+    for number in (1..4).rev(){
+        println!("{}",number);
+    }
+
+    println!("LIFTOFF!!!");
 }
+// fn Fire_number(){
+//     let mut number = 3;
+
+//     while number != 0{
+//         println!("{}!",number);
+
+//         number -= 1;
+//     }
+//     println!("LIFTOFF!");
+// }
---
+++ readme.md
@@ -0,0 +1,13 @@
+# RUST開発
+
+[rustの公式日本語ドキュメント](https://doc.rust-jp.rs/book-ja/title-page.html)
+
+[競プロ](https://atcoder.jp/contests/abs)
+
+A260111 == 公式ドキュメント 2 ~ 3.4
+
+AtCoder == 競プロ用
+
+B260111 == 公式ドキュメント 3.5
+
+hello_cargo == 公式ドキュメント 1.3
