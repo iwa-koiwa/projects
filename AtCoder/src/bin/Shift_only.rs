96f2fd605d582dab0b7b2cb17c1905fe920f5bbe
iwa-koiwa <220993652+iwa-koiwa@users.noreply.github.com>
2026-01-20 01:31:02 +0900

    Shift_onlyを作成

---
+++ AtCoder/src/bin/Shift_only.rs
@@ -0,0 +1,16 @@
+use proconio::input;
+
+fn main() {
+    input! {
+    n: usize,
+    mut a : [i32; n],
+    }
+    let mut total = 0;
+    for i in 0..n{
+        if a[i] % 2 == 1{
+            break;
+        }
+        total = i;
+    }
+    println!("{}",total);
+}
