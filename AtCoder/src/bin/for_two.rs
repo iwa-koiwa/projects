c1956096d47d6dd03c947d6e7407f60dfee1218c
iwa <220993652+iwa-koiwa@users.noreply.github.com>
2026-01-21 08:52:24 +0900

    fortwo変更

--- AtCoder/src/bin/for_two.rs
+++ AtCoder/src/bin/for_two.rs
@@ -3,21 +3,13 @@ use proconio::input;
 fn main(){
     input! {
         n:u16,
+        a:[u16;n],
     }
-    let mut a_total =0;
+    let a_sors = a.sort();
     for i in 0..n{
         if i%2==0{
-            input! {
-                a:i16
-            }
-            a_total += a;
         }
         else if i%2 == 1 {
-            input! {
-                a:i16
-            }
-            a_total -= a;
         }
     }
-    println!("{}",a_total.abs())
 }
