d09b9dcc2d76435f6b5d9236518241a88359052c
iwa <220993652+iwa-koiwa@users.noreply.github.com>
2026-01-20 17:25:26 +0900

    ちゃんと動く用修正

--- AtCoder/src/bin/Some_sums.rs
+++ AtCoder/src/bin/Some_sums.rs
@@ -9,9 +9,9 @@ fn main() {
     let n_limit:[u64;5] =[1,10,100,1000,10000];
     let mut total = 0;
     for i in 1..=n{
-        let mut k =0;
-        for j in 1..i.to_string().chars().count(){
-            k = i % n_limit[j];
+        let mut k = 0;
+        for j in 0..i.to_string().chars().count(){
+            k += (i / n_limit[j]) % 10;
         }
         if k >= a && k <=b {
             total += i;
