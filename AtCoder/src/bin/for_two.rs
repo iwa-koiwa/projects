226e67559d0fb934211a4db31bacaf83bd612149
iwa <220993652+iwa-koiwa@users.noreply.github.com>
2026-01-21 15:08:12 +0900

    fow twoを動くように変更した

--- AtCoder/src/bin/for_two.rs
+++ AtCoder/src/bin/for_two.rs
@@ -2,14 +2,20 @@ use proconio::input;
 
 fn main(){
     input! {
-        n:u16,
-        a:[u16;n],
+        n:i16,
+        mut a:[i16;n],
     }
-    let a_sors = a.sort();
-    for i in 0..n{
-        if i%2==0{
+    let mut a_total = 0;
+    a.sort();
+    for i in 0..n as usize {
+        if  i%2 == 0 {
+            let a1 = a[i];
+            a_total += a1;
         }
-        else if i%2 == 1 {
+        else if i%2==1 {
+            let a2 =a[i];
+            a_total -= a2;
         }
     }
+    println!("{}",a_total.abs());
 }
